57ed1c253d19bb6dc0f79905b6bc6b0c7fdc5bcb
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2023-03-20 17:02:42 +0100

    prato fiorito

--- CampoMinato/app/src/main/java/com/example/campominato/MainActivity.java
+++ CampoMinato/app/src/main/java/com/example/campominato/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.campominato;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +25,129 @@ public class MainActivity extends AppCompatActivity {
 
         griglia = (GridLayout) findViewById(R.id.griglia);
         lista = griglia.getTouchables().stream().map(b -> (Button) b).collect(Collectors.toList());
+
+        generaCampo();
+    }
+
+    /**
+     * SI CALCOLA LA DISTANZA DALLE BOMBE per ogni bottone
+     */
+    public void generaCampo() {
+        piazzaBombe();
+        for(Button b : lista) {
+            int idx = lista.indexOf(b);
+            int buff = 0;
+
+
+            if(b.getTag().toString().equals("bomba")) {
+                continue;
+            }
+
+            for(int i = 1; i <= 5; i++) {
+                if(idx % 4 == 3){
+                    if(i == 1 || i == 2 ||i == 5) {
+                        continue;
+                    }
+                }
+
+                if(idx % 4 == 0){
+                    if( i == 2 || i == 3) {
+                        continue;
+                    }
+                }
+
+                if(idx % 4 == 2){
+                    if( i == 2) {
+                        continue;
+                    }
+                }
+
+                if(idx % 4 == 1){
+                    if( i == 2) {
+                        continue;
+                    }
+                }
+
+                if(i == 2) {
+                    continue;
+                }
+
+                if((idx+i) >= lista.size()) {
+                    continue;
+                }
+
+                if(lista.get(i+idx).getTag().toString().equals("bomba")){
+                    buff++;
+                }
+            }
+
+            if(idx <= 0){
+                b.setTag(buff);
+                continue;
+            }
+
+
+            for(int i = -1; i >= -5; i--){
+                if((idx+i) < 0) {
+                 continue;
+                }
+
+                if(idx % 4 == 0){
+                    if(i == -1 || i == -2 || i == -5) {
+                        continue;
+                    }
+                }
+
+                if(idx % 4 == 1){
+                    if( i == -2) {
+                        continue;
+                    }
+                }
+
+                if(idx % 4 == 2){
+                    if( i == -2) {
+                        continue;
+                    }
+                }
+
+                if(idx % 4 == 3){
+                    if(i == -2 || i == -3) {
+                        continue;
+                    }
+                }
+
+                if(lista.get(idx+i).getTag().toString().equals("bomba")){
+                    buff++;
+                }
+
+
+            }
+            b.setTag(buff);
+        }
+    }
+
+    /**
+     * piazza casualmente le bombe sul campo
+     */
+    public void piazzaBombe() {
+        int i = 0;
+        for(; i < 4; i++){
+            Random ra = new Random();
+            int interoGenerato = ra.ints(0, 15)
+                    .findFirst()
+                    .getAsInt();
+            System.out.println(interoGenerato);
+            Button x = lista.get(interoGenerato);
+            if(x.getTag().toString().equals("bomba")) {
+                System.out.println("numero kkkk \n");
+                interoGenerato = ra.ints(0, 15)
+                        .findFirst()
+                        .getAsInt();
+                x = lista.get(interoGenerato);
+            }
+            x.setTag("bomba");
+        }
+
     }
 
     public void clicca(View v) {
@@ -37,4 +162,11 @@ public class MainActivity extends AppCompatActivity {
         ((Button) v).setText(v.getTag().toString());
         return;
     }
+
+    public void reset(View v) {
+        lista.forEach(s -> s.setText(""));
+        lista.forEach(s->s.setEnabled(true));
+        generaCampo();
+    }
 }
+
